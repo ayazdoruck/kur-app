43352d5d0489fa701a5b1db2b5eee22a9c643746
pahiy <ayazdoruck@gmail.com>
2025-08-16 00:43:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/kur")
+def kur():
+    r = requests.get("https://finans.truncgil.com/v4/today.json")
+    return jsonify(r.json())
+
+app.run(host="0.0.0.0", port=10000)
 
